8388c82cc5c0c0e59cb7cfcbb960fe564db7e7cf
Dmitry <dmitryt0karev@yandex.ru>
2024-04-05 00:27:18 +0300

    add Cat and HelloWorld

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        Cat cat = applicationContext.getBean("catBean", Cat.class);
+        Cat cat2 = applicationContext.getBean("catBean", Cat.class);
+
+        System.out.println("Переменные bean и bean2 ссылаются на один и тот же объект? " + (bean == bean2));
+        System.out.println("Переменные cat и cat2 ссылаются на один и тот же объект? " + (cat == cat2));
     }
 }
